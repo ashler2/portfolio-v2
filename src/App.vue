1e5dd5c805eb50eb56bec7cb0d9358a1902eb9a1
Ash <ashley.rt@live.co.uk>
2023-04-02 20:01:24 +0100

    Added the about section, updated formatting, added a section heading component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
 import Navbar from "@/components/nav/Navbar.vue";
 import Hero from "@/components/sections/Hero.vue";
 import EmailLink from "@/components/misc/EmailLink.vue";
+import About from "@/components/sections/About.vue";
 </script>
 
 <template>
   <header class="header" ref="header">
-    <Navbar />
+    <Navbar/>
   </header>
   <main>
     <Hero/>
+    <About/>
   </main>
-  <EmailLink />
+  <EmailLink/>
 
 </template>
 
---
+++ src/components/sections/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <SectionHeading title="About Me" number="01"/>
+
+        </div>
+        <div class="col-sm-6">
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import SectionHeading from "@/components/misc/SectionHeading.vue";
+
+export default {
+  name: "About",
+  components: {SectionHeading}
+}
+</script>
+
+<style scoped>
+
+</style>
