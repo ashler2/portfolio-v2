5e1ebaa56ea0d3fd1c00dc79831799411f10e122
Ash <ashley.rt@live.co.uk>
2023-04-03 21:50:03 +0100

    Added a basic about section, added a list item vue component and styles.

---
+++ src/components/misc/ListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="col-sm-3 list-item">
+    <span>{{ label }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+  },
+}
+</script>
