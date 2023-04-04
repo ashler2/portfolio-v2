cd87b12e9290cac91a5d386e65bcfebbef26e86f
Ash <ashley.rt@live.co.uk>
2023-04-04 22:47:39 +0100

    Added a experince vue section, added a heading.

---
+++ src/components/sections/Experience.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row">
+        <SectionHeading title="Experience" number="02"/>
+      </div>
+      <div class="row">
+        <div class="tab-panel">
+          
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import SectionHeading from "@/components/misc/SectionHeading.vue";
+
+export default {
+  name: "Experience",
+  components: {SectionHeading}
+}
+</script>
+
+<style scoped>
+
+</style>
