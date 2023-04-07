8626288ad89c662a664c7c2d1f645833cad112f1
Ash <ashley.rt@live.co.uk>
2023-04-07 18:32:58 +0100

    Added the projects section

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Navbar from "@/components/nav/Navbar.vue";
 import Hero from "@/components/sections/Hero.vue";
 import EmailLink from "@/components/misc/EmailLink.vue";
 import About from "@/components/sections/About.vue";
+import Projects from "@/components/sections/Projects.vue";
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import About from "@/components/sections/About.vue";
   <main>
     <Hero/>
     <About/>
+    <Projects/>
   </main>
   <EmailLink/>
 
